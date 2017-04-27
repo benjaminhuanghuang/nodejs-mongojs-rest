add6c38bee9dad371840adc05b4fb24b29e766d8
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-26 21:25:19 -0700

    update

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
     > use catalog
     > db.createCollection('products')
     > db.products.insert({name:'Fire Table', category:'Electronics', model:'B00TSUGXKE', price:67.99, image:'', details:''})
+
+## Enable CORS
+    https://enable-cors.org/server_expressjs.html
--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 const port = 3600;
 
 app.use(bodyParser.json());
+//Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res, next) => {
     res.send("Hello Node.js");
